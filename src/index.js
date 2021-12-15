3363c90b21c50ab3609bd21558455ac3a1f34d81
shadow <adebayomutalib5@gmail.com>
2021-12-15 11:34:33 +0100

    portofolio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
-
+import emailjs from 'emailjs-com';
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+const selectForm = document.querySelector("form")
+const button = document.querySelector('button')
+const allInput = document.querySelectorAll("input")
+selectForm.addEventListener('submit',() =>{
+
+    button.textContent= "MESSAGE SENT"
+
+})
+
+ 
